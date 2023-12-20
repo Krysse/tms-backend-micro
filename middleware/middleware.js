48042e59bc3159c75081758cd6dde689131dbd34
Jeremiah <therealjeremiah@gmail.com>
2023-12-20 16:19:22 +0800

    Changed PS002 => AS002

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -3,10 +3,9 @@ exports.catchSpecialCharacters = (req, res, next) => {
   const regex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/
   //we should strip the first /
   //Check if the endpoint contains special characters
-  console.log(req.url.slice(1))
   if (regex.test(req.url.slice(1))) {
     return res.json({
-      code: "PS002"
+      code: "AS002"
     })
   }
   next()
